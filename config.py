e68b2bbfe2e249a09f2dfc067e9fdfb7cc1a840d
Wayne <regocodes@gmail.com>
2026-05-08 14:53:56 +0530

    Add configuration file for pre-set server setup

---
+++ config.py
@@ -0,0 +1,32 @@
+# Configuration for Discord Nuke Bot
+# Set your desired channels, roles, and server name here
+# The bot will create these after nuking
+
+# Server name after nuke
+SERVER_NAME = "My Awesome Server"
+
+# Channels to create (list of channel names)
+CHANNELS = [
+    "welcome",
+    "announcements",
+    "general",
+    "random",
+    "memes",
+    "voice-chat"
+]
+
+# Roles to create (list of role names)
+ROLES = [
+    "Admin",
+    "Moderator",
+    "Member",
+    "Verified"
+]
+
+# Optional: Set welcome message
+WELCOME_MESSAGE = """
+🎉 **Welcome to the freshly nuked server!**
+
+This server has been completely reset and rebuilt.
+Check the roles and channels to get started!
+"""
